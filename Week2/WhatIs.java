79dac955c5ffbf43bd59baa114fd68afb1289eb4
Karim <32221969+VolcanicSlippers@users.noreply.github.com>
2017-10-23 08:15:47 +0100

    Create WhatIs.java

---
+++ Week2/WhatIs.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ * @Author: KarimLearnsToCode
+ * @Date: 08/10/2017
+ * this code will allow us to prin out "What is this program?"
+ * 
+ * 
+ */
+public class WhatIs {
+	public static void main(String[] args) {
+		System.out.println("What is this Program?");
+	}
+}
